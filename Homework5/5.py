98c8cf048e9bcfe55f7d7f9784bf1001d663da08
karina210 <karinav19817@gmail.com>
2021-02-26 21:19:00 +0300

    first commit

---
+++ Homework5/5.py
@@ -0,0 +1,66 @@
+# a = [1, 2, 9, 6, 3, 4]
+# for i in a:
+#     if i < 5:
+#         print(i)
+
+# a = [1, 2, 3, 6, 3, 5, 9]
+# b = [1, 6, 4, 3, 5, 4]
+# for i in a:
+#     for j in b:
+#         if i == j:
+#             print(i)
+
+
+# a = {1:10, 2:12, 5:15, 6:16, 3:13, 4:14}
+# b = sorted(dict.values(a))
+# print(b)
+
+
+# print(int('ABC', 25))
+
+
+# def palindrom(string):
+#     return string == string[::-1]
+# print(palindrom('anna'))
+
+
+
+# def a(n):
+#     n1 = int(n)
+#     n2 = int(n) ** 2
+#     n3 = int(n) ** 3
+#     n4 = n1 + n2 + n3
+#     print(n4)
+# print(a(6))
+
+# a = [2, 6, 45, 73, 237, 2596]
+# for i in a:
+#     if i == 237:
+#         break
+#         i += 1
+#     print(i)
+
+
+
+# a = set([1, 3, 5, 7])
+# b = set([1, 6, 7, 4])
+# print(a - b)
+
+# a = "hfyhfvkjifjih hvghy6g"
+# result_a = a.replace('h', 'k')
+# print(result_a)
+# b = a.count('a')
+# print(b)
+
+
+# x = 5
+# y = 6
+# x, y = y, x
+# print(x, y)
+
+
+# a = [1, 6, 18, 7, 21 ]
+# for i in a:
+#     if i % 3 == 0:
+#       print(i)
+
